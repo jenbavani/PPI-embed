e8bc6c6b457207039d79a897d3e4b3ed13f6803c
J. Bavani Kehoe <jenbavani@yahoo.com>
2012-05-29 19:25:52 -0700

    cleaned + commented + 1 bug fixed
    line 26: s / ==k / <=k
    (kth order common neighbors are *within* k, not exactly k)

--- dthOrdCommonNeighbors.m
+++ dthOrdCommonNeighbors.m
@@ -1,11 +1,29 @@
-function [MAT] = dthOrdCommonNeighbors(ADJMAT,d) 
+function [MAT] = dthOrdCommonNeighbors(ADJMAT,D)
 
-APSP = graphallshortestpaths(ADJMAT);
-n = size(APSP,1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Returns an n-by-n-by-d matrix that contains the number of dth order 
+% common neighbors each pair of nodes has, from d = 1 to D.
+%
+% dth order neighbors are nodes which are within a graph distance of d 
+%     from both nodes.
+%
+% INPUTS:
+% ADJMAT = the graph under consideration, in sparse matrix form
+% D = the max order (d) to consider
+%
+% OUTPUT:
+% [MAT] = dthOrdCommonNeighbors(ADJMAT,D)
+%  --> MAT(i,j,k) = the # of kth order common neighbors shared by nodes
+%                   i and j.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+apsp = graphallshortestpaths(ADJMAT);
+n = size(apsp,1);
 pathmat = zeros(n);
 
-for k = 1:d
-    pathmat(APSP==k)=1;
+for k = 1:D
+    pathmat(apsp<=k)=1;
     numNeighbors = @(i,j) sum( pathmat(i,:).*pathmat(j,:) );
     for i = 1:n
         for j = 1:n
